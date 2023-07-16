c8f6cd850d6dd208e2ffde5952c5598262858259
marcavis <marcavis@yahoo.com.br>
2023-07-16 18:18:15 -0300

    new stuff

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the map container element
+    const mapContainer = document.getElementById('map-container');
+  
+    // Load the SVG file using an AJAX request
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'sc.svg', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Insert the SVG code into the map container
+        mapContainer.innerHTML = xhr.responseText;
+  
+        // Get the SVG map element
+        const map = document.querySelector('svg');
+  
+        // Add click event listener to the map
+        map.addEventListener('click', function(event) {
+          // Get the clicked path element
+          const clickedPath = event.target;
+  
+          // Log the ID of the clicked municipality
+          console.log('Clicked municipality:', clickedPath.id);
+        });
+      }
+    };
+    xhr.send();
+  });
